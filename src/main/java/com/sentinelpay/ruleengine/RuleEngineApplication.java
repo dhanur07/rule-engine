a672b8e0a4354b2356d230238f1424445175bfd8
Dhanur Motwani <55673796+dhanur07@users.noreply.github.com>
2026-06-15 16:21:07 -0400

    Update RuleEngineApplication.java
    feat: add Kafka Streams topology with Avro serialization

--- src/main/java/com/sentinelpay/ruleengine/RuleEngineApplication.java
+++ src/main/java/com/sentinelpay/ruleengine/RuleEngineApplication.java
@@ -2,8 +2,13 @@ package com.sentinelpay.ruleengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.sentinelpay.ruleengine",
+        "com.sentinelpay.common"
+})
 public class RuleEngineApplication {
 
 	public static void main(String[] args) {
